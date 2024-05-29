72ec67e8ff38b40fdc170683fc54e65544256ea1
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-05-29 17:43:36 +0100

    chore: Update npm dependencies and add GraphQL module
    This commit updates the npm dependencies in the `package.json` file, ensuring that the project is using the latest stable versions of the dependencies. It also adds the `@nestjs/graphql` module and configures it in the `app.module.ts` file. This module enables GraphQL support in the application and sets up the Apollo server with the provided configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { HelloworldModule } from './helloworld/helloworld.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloworldModule,
+  ],
   controllers: [],
   providers: [],
 })
