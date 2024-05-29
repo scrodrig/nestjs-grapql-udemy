49881ab8f20665d0bbf2db2d50021f6a78b19bb5
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-05-29 18:32:01 +0100

    chore: Disable GraphQL playground and add Apollo Server landing page plugin
    This commit modifies the `app.module.ts` file to disable the GraphQL playground by setting the `playground` property in the `GraphQLModule.forRoot` configuration to `false`. Additionally, it adds the `ApolloServerPluginLandingPageLocalDefault` plugin to the `plugins` array in the same configuration. This plugin enables the Apollo Server landing page, providing a user-friendly interface for exploring and testing the GraphQL API. These changes improve the development experience and align with the recent updates to the npm dependencies and GraphQL support in the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HelloworldModule } from './helloworld/helloworld.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: true,
+      playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault() as any],
     }),
     HelloworldModule,
   ],
