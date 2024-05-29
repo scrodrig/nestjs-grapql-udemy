952d99d8c63aa44751d9a2bdeb2a2248854970d0
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-05-29 22:28:40 +0100

    cors commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://studio.apollographql.com' });
+  app.enableCors({ origin: ['https://studio.apollographql.com'] });
   await app.listen(3000);
 }
 bootstrap();
