058293469297967d11e2aca654f9b291d5b49948
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-05-31 10:33:21 +0100

    feat: Update todos resolver to use TodosService
    This commit updates the todos resolver in the todos.resolver.ts file to utilize the TodosService. Instead of returning an empty array of todos, the resolver now calls the findAll() method of the TodosService to retrieve the list of todos. This enhancement improves the functionality of the application by integrating the TodosService into the resolver and ensuring that the actual data is returned.

--- src/todos/todos.resolver.ts
+++ src/todos/todos.resolver.ts
@@ -1,13 +1,16 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
+import { TodosService } from './todos.service';
 
 @Resolver()
 export class TodosResolver {
+  constructor(private readonly todosService: TodosService) {}
+
   @Query(() => [Todo], {
     name: 'todos',
     description: 'Returns a list of todos',
   })
   findAll(): Todo[] {
-    return [];
+    return this.todosService.findAll();
   }
 }
