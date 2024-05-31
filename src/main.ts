c302eada360ff0bf372014c8df74f33dcf5a91a4
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-05-31 22:17:16 +0100

    feat: Add global validation pipe to main.ts
    This commit adds a global validation pipe to the `main.ts` file. The pipe is created using the `ValidationPipe` class from the `@nestjs/common` package. It is configured to whitelist incoming data and forbid non-whitelisted properties. This enhancement improves the reliability and integrity of the application by validating user input globally.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['https://studio.apollographql.com'] });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
