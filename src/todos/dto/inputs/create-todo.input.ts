42704170175ffd8d6a950f5921e0182cae752484
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-05-31 22:17:11 +0100

    feat: Add validation to CreateTodoInput description field
    This commit adds validation to the `description` field of the `CreateTodoInput` class in the `create-todo.input.ts` file. The `IsString` decorator ensures that the value is a string, while the `IsNotEmpty` decorator ensures that the value is not empty. Additionally, the `MaxLength` decorator limits the length of the description to 30 characters. This enhancement improves the reliability and integrity of the application by validating user input for creating todos.

--- src/todos/dto/inputs/create-todo.input.ts
+++ src/todos/dto/inputs/create-todo.input.ts
@@ -1,7 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput {
   @Field(() => String, { description: 'The description of the todo' })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   description: string;
 }
